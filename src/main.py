184b4cacf123bd9352a35d2f5dc48f0cafa73c6a
Rudra <rudra.sai@neewee.ai>
2026-03-07 21:21:33 +0530

    Modules and Packages

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from package import calc,mul
+from package.subpackage import division
+
+print(calc.add(2,3))
+
+
+print(calc.subtract(2,3))
+
+
+print(mul.mult(10,30))
+
+
+print(division.division(20,10))
